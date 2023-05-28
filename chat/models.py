e42f61132e0b816ffeb96c68132ca602f3a21d7b
fattymango <malak.jkl@gmial.com>
2023-05-28 12:49:34 +0300

    fix chat rooms

--- chat/models.py
+++ chat/models.py
@@ -17,7 +17,7 @@ class Message(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='message_author')
 	room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, null=True)
 	message = models.CharField(max_length=100, blank=True, null=True)
-	date_created = models.DateField(auto_now_add=True, blank=True, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
 	def __str__(self):
 		return f'{self.author.full_name} - {self.message}'
